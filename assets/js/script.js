8ed4c665aa1bde6392c648b605e5621928826ea5
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-04-14 16:04:35 +0100

    section 1 intro animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,16 @@ const navLinks = document.querySelector(".nav-bar");
 
 window.addEventListener("load", function (event) {
   event.preventDefault(); //on page load, unintended behaviour of section 1 title expanding to full size. this effect not intended.
-  introReveal.forEach(function (spn) {
-    spn.classList.remove("title-hide");
-    spn.classList.add("title-reveal");
+  introReveal.forEach(function (span, i) {
+    setTimeout(() => {
+      span.classList.remove("title-hide");
+      span.classList.add("title-reveal");
+    }, 1500 * i);
   });
 });
 
 navLinks.addEventListener("click", function (event) {
-  event.preventDefault();
+  event.preventDefault(); //needed or smooth scrolling wont work. will instantly go onto next section.
   if (event.target.classList.contains("nav-link")) {
     const linkSection = event.target.getAttribute("href");
     document.querySelector(linkSection).scrollIntoView({ behavior: "smooth" });
