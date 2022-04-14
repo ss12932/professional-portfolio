074926dac6b5614e8c826aac39cb20a708fce6a2
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-04-14 15:10:52 +0100

    added js. intro load + smooth scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+const introReveal = document.querySelectorAll(".intro-title");
+const navLinks = document.querySelector(".nav-bar");
+
+window.addEventListener("load", function (event) {
+  event.preventDefault(); //on page load, unintended behaviour of section 1 title expanding to full size. this effect not intended.
+  introReveal.forEach(function (spn) {
+    spn.classList.remove("title-hide");
+    spn.classList.add("title-reveal");
+  });
+});
+
+navLinks.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (event.target.classList.contains("nav-link")) {
+    const linkSection = event.target.getAttribute("href");
+    document.querySelector(linkSection).scrollIntoView({ behavior: "smooth" });
+  }
+});
